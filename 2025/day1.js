9335743172cd2983e969dd91f96cee89d420391d
TJ Thielen <tthielen@sourceallies.com>
2025-12-02 16:30:01 -0600

    fix my 2025

--- 2025/day1.js
+++ 2025/day1.js
@@ -23,53 +23,30 @@ function p1() {
 }
 
 function p2(){
-    let pos1 = 50;
-    let total1 = 0;
-
-    let pos2 = 50;
-    let total2 = 0;
+    let pos = 50;
+    let total = 0;
 
     for(const line of lines){
         const num = Number(line.substring(1));
         if(line[0] == 'R'){
-            pos1 += num;
-        } else {
-            if(pos1 == 0){
-                pos1 = 100;
-            }
-            pos1 -= num;
-            if(pos1 % 100 == 0){
-                pos1 -= 100;
-            }
-        }
-        
-        total1 += Math.abs(Math.floor((1000 + pos1) / 100) - 10);
-        pos1 = (1000 + pos1) % 100;
-
-        
-        if(line[0] == 'R'){
             for(let i = 0; i < num; i++){
-                pos2 += 1;
-                pos2 = pos2 % 100;
-                if(pos2 == 0){
-                    total2++;
+                pos += 1;
+                pos = pos % 100;
+                if(pos == 0){
+                    total++;
                 }
             } 
         } else {
             for(let i = 0; i < num; i++){
-                pos2 -= 1;
-                pos2 = (pos2 + 100) % 100;
-                if(pos2 == 0){
-                    total2++;
+                pos -= 1;
+                pos = (pos + 100) % 100;
+                if(pos == 0){
+                    total++;
                 }
             }
         }
-        if(total1 != total2){
-            console.log();
-        }
     }
-    console.log(total1);
-    console.log(total2);
+    console.log(total);
 
 }
 
