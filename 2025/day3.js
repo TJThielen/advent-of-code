53aaca825f0463c4c3ae3cf9e95c6b3bab2150c2
TJ Thielen <tthielen@sourceallies.com>
2025-12-02 23:52:07 -0600

    refactored p1

--- 2025/day3.js
+++ 2025/day3.js
@@ -6,26 +6,12 @@ const lines = content.split('\n');
 
 function p1(){
     let total = 0;
-    for(const line of lines){
-        let largest = 0;
-        let largestIndex = 0;
-        for(let i = 0; i < line.length; i++){
-            largestIndex = Number(largest) < Number(line.charAt(i)) ? i : Number(largestIndex);
-            largest = Number(largest) < Number(line.charAt(i)) ? Number(line.charAt(i)) : Number(largest);
-        }
-
-        let nextLargest = 0;
-        if(largestIndex !== line.length - 1){
-            for(let i = largestIndex + 1; i < line.length; i++){
-                nextLargest = Number(nextLargest) < Number(line.charAt(i)) ? line.charAt(i) : Number(nextLargest);
-            }
-            total += Number(largest.toString() + nextLargest.toString());
-        } else {
-            for(let i = 0; i < largestIndex; i++){
-                nextLargest = Number(nextLargest) < Number(line.charAt(i)) ? line.charAt(i) : Number(nextLargest);
-            }
-            total += Number(nextLargest.toString() + largest.toString());
-        }
+    for(let line of lines){
+        let max = '';
+        const largest = getLargest(line.substring(0, line.length-1));
+        const nextLargest = getLargest(line.substring(largest[1]+1));
+        max = largest[0].toString() + nextLargest[0].toString();
+        total += Number(max);
     }
     console.log(total);
 }
