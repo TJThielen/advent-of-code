8890ab06f4dd78f66a9e60401cab5b4be8fb1568
TJ Thielen <tthielen@sourceallies.com>
2025-12-09 16:58:32 -0600

    cleaned up

--- 2025/day9.js
+++ 2025/day9.js
@@ -3,7 +3,6 @@ import {readFile} from 'fs/promises';
 // const file = await readFile('./test.txt', 'utf8')
 const file = await readFile('./day9.txt', 'utf8')
 let coords = file.split('\n').map(line => line.split(',')).map(line => [Number(line[0]), Number(line[1])]);
-let walls = [];
 
 function p1(){
     let rectangleArea = 0;
@@ -27,70 +26,12 @@ function p2(){
 
     const sortedAreas = [...areas].sort((a, b) => b[1] - a[1]);
 
-    a:
     for(const area of sortedAreas){
-        for(let index = 0; index < coords.length; index++){
-            let coord = coords[index];
-            const [inMiddle, onEdge] = existsInArea(area[0], coord)
-            if(inMiddle){
-                continue a;
-            } else if(onEdge){
-                let lastIndex = index - 1;
-                lastIndex = lastIndex < 0 ? coords.length-1 : lastIndex;
-                let nextIndex = (index + 1) % coords.length;
-                if(cutsAcross(area[0], index, lastIndex) || cutsAcross(area[0], index, nextIndex)){
-                    continue a;
-                }
-            }
-        }
         if(isGreen(area[0])){
             console.log(area);
             return;
         }
     }
-}
-
-function existsInArea(area, coord){
-    let points = area.split(' ').map(a => a.split(',')).map(arr => [Number(arr[0]), Number(arr[1])]);
-
-    if((points[0][0] == coord[0] && points[0][1] == coord[1]) || (points[1][0] == coord[0] && points[1][1] == coord[1])) {
-        return [false, false]
-    }
-
-    //in or on edge of area
-    if((coord[0] <= points[0][0] && coord[0] >= points[1][0]) || (coord[0] >= points[0][0] && coord[0] <= points[1][0])){
-        if((coord[1] <= points[0][1] && coord[1] >= points[1][1]) || (coord[1] >= points[0][1] && coord[1] <= points[1][1])){
-            
-            //on edge
-            if(coord[0] == points[0][0] || coord[0] == points[1][0] || coord[1] == points[0][1] || coord[1] == points[1][1]){
-                return [false, true];
-            }
-            
-            return [true, null];
-        }
-    }
-    
-    return [false, false];
-}
-
-function cutsAcross(area, thisIndex, nextIndex){
-    let thisPoint = [...coords[thisIndex]];
-    let nextPoint = [...coords[nextIndex]];
-    let newPoint = [...coords[thisIndex]];
-
-    //move 1 square towards next point and recheck
-    if(nextPoint[0] == thisPoint[0] && nextPoint[1] < thisPoint[1]){
-        newPoint[1]--;
-    } else if(nextPoint[0] == thisPoint[0] && nextPoint[1] > thisPoint[1]){
-        newPoint[1]++;
-    } else if(nextPoint[1] == thisPoint[1] && nextPoint[0] > thisPoint[0]){
-        newPoint[0]++;
-    } else if(nextPoint[1] == thisPoint[1] && nextPoint[0] < thisPoint[0]){
-        newPoint[0]--;
-    }
-
-    const res = existsInArea(area, newPoint);
-    return res[0];
 }
 
 function isGreen(area){
@@ -125,9 +66,10 @@ function isGreen(area){
             endWall = temp;
         }
 
-        if(startWall[1] < startRangeY && endWall[1] > endRangeY && startWall[0] > startRangeX && startWall[0] < endRangeX){
+        if(startWall[1] < endRangeY && endWall[1] > startRangeY && startWall[0] > startRangeX && startWall[0] < endRangeX){
             return false;
         }
+
     }
 
     //horizontal line check
@@ -141,7 +83,7 @@ function isGreen(area){
             endWall = temp;
         }
 
-        if(startWall[0] < startRangeX && endWall[0] > endRangeX && startWall[1] > startRangeY && startWall[1] < endRangeY){
+        if(startWall[0] < endRangeX && endWall[0] > startRangeX && startWall[1] > startRangeY && startWall[1] < endRangeY){
             return false;
         }
     }
